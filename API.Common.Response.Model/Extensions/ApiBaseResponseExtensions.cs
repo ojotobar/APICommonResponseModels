6db3b2fd5a67f941f4151c945c9f2ba4a0afaf88
Toba Ojo <ojotobar@gmail.com>
2025-08-14 20:13:26 +0100

    Added CI/CD, response data getter and license

---
+++ API.Common.Response.Model/Extensions/ApiBaseResponseExtensions.cs
@@ -0,0 +1,10 @@
+﻿using API.Common.Response.Model.Responses;
+
+namespace API.Common.Response.Model.Extensions
+{
+    public static class ApiBaseResponseExtensions
+    {
+        public static TResult GetResult<TResult>(this ApiBaseResponse response) =>
+            ((OkResponse<TResult>)response).Result;
+    }
+}
