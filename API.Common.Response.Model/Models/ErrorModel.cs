2f306e476ca5daf74caea499d5e43c4c09231165
Toba Ojo <ojotobar@gmail.com>
2025-08-14 21:23:37 +0100

    Added `ProcessError()` method to to process and return frmatted error responses

---
+++ API.Common.Response.Model/Models/ErrorModel.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Common.Response.Model.Models
+{
+    public class ErrorModel
+    {
+        public string Message { get; set; } = string.Empty;
+        public int StatusCode { get; set; }
+    }
+}
